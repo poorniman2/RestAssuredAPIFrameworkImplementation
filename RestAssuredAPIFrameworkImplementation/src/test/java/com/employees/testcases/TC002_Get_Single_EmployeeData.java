b8d3b40b2bd79c37ae68b58b11aab47db7b5518c
PoornimaN2 <PoornimaN2@IMCBBCP54-BLL.mumbai1.corp.citiustech.com>
2022-05-05 12:20:48 +0530

    first commit

---
+++ RestAssuredAPIFrameworkImplementation/src/test/java/com/employees/testcases/TC002_Get_Single_EmployeeData.java
@@ -0,0 +1,88 @@
+package com.employees.testcases;
+
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.employees.base.TestBase;
+
+import io.restassured.RestAssured;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
+public class TC002_Get_Single_EmployeeData extends TestBase{
+	RequestSpecification httpRequest;
+	Response response;
+		
+	@BeforeClass
+	void getEmployeeData() throws InterruptedException
+	{
+		logger.info("*********Started TC002_Get_Single_Employee_Record **********");
+		
+		RestAssured.baseURI = "http://dummy.restapiexample.com/api/v1";
+		httpRequest = RestAssured.given();
+		response = httpRequest.request(Method.GET, "/employee/1");
+		
+		Thread.sleep(7000);
+	}
+	
+	@Test
+	void checkResposeBody()
+	{
+		String responseBody = response.getBody().asString();
+		Assert.assertEquals(responseBody.contains("1"), true);
+	}
+		
+	@Test
+	void checkStatusCode()
+	{
+		int statusCode = response.getStatusCode(); // Gettng status code
+		Assert.assertEquals(statusCode, 200);
+	}
+	
+	@Test
+	void checkResponseTime()
+	{
+		long responseTime = response.getTime(); // Getting status Line
+		Assert.assertTrue(responseTime<6000);
+		
+	}
+		
+	@Test
+	void checkstatusLine()
+	{
+		String statusLine = response.getStatusLine(); // Gettng status Line
+		Assert.assertEquals(statusLine, "HTTP/1.1 200 OK");
+		
+	}
+	
+	@Test
+	void checkContentType()
+	{
+		String contentType = response.header("Content-Type");
+		Assert.assertEquals(contentType, "text/html; charset=UTF-8");
+	}
+
+	@Test
+	void checkserverType()
+	{
+		String serverType = response.header("Server");
+		Assert.assertEquals(serverType, "nginx/1.14.1");
+	}
+
+	@Test
+	void checkContentLenght()
+	{
+		String contentLength = response.header("Content-Length");
+		Assert.assertTrue(Integer.parseInt(contentLength)<1500);
+	}
+	
+	@AfterClass
+	void tearDown()
+	{
+		logger.info("*********  Finished TC002_Get_Single_Employee_Record **********");
+	}
+	
+}
